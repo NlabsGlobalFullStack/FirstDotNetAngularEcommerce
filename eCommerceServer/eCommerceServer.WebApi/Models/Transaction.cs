817b889ecc1ca545ea404b18998ef44da9b9c653
Cuma KÖSE <turkmvc@gmail.com>
2024-01-20 21:09:25 +0300

    Update Transaction.cs

--- eCommerceServer/eCommerceServer.WebApi/Models/Transaction.cs
+++ eCommerceServer/eCommerceServer.WebApi/Models/Transaction.cs
@@ -7,6 +7,5 @@ public sealed class Transaction : BaseModel
     public Guid SellerId { get; set; }
     public Seller? Seller { get; set; }
     public byte TransactionType { get; set; } = 0; // Sale Or Buy // Sale/Satış => 0 / Buy/Alış => 1
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
     public IEnumerable<TransactionDetail>? Details { get; set; }
 }
