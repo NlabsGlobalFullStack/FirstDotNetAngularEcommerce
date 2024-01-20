3335d017ccc965356290f4197c79e09274ced9d4
Cuma KÖSE <turkmvc@gmail.com>
2024-01-20 21:10:14 +0300

    Update Seller.cs

--- eCommerceServer/eCommerceServer.WebApi/Models/Seller.cs
+++ eCommerceServer/eCommerceServer.WebApi/Models/Seller.cs
@@ -5,6 +5,5 @@ public class Seller : BaseModel
     public Guid UserId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
     public IEnumerable<Product> Products { get; set; }
 }
