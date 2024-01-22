7a24844dbe2b82e0975202b5bf4f0b587ec28762
Cuma KÖSE <turkmvc@gmail.com>
2024-01-22 20:44:28 +0300

    Delete eCommerceServer/eCommerceServer.WebApi/Models/Seller.cs

--- eCommerceServer/eCommerceServer.WebApi/Models/Seller.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ECommerceServer.WebApi.Models;
-
-public class Seller : BaseModel
-{
-    public Guid UserId { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Description { get; set; } = string.Empty;
-    public IEnumerable<Product> Products { get; set; }
-}
